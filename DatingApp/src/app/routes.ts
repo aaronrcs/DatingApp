2d7b55c954e1eca108dd557846b96eb73acccb91
aaronrcs <aromer61@calstatela.edu>
2020-02-16 23:52:47 -0800

    Added AuthGaurd to all current components

--- DatingApp/src/app/routes.ts
+++ DatingApp/src/app/routes.ts
@@ -6,10 +6,17 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
-    { path: 'messages', component: MessagesComponent },
-    { path: 'lists', component: ListsComponent },
-    // Using wildcard path in case other paths don't match
-    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'home', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent }
+    ]
+  },
+  // Using wildcard path in case other paths don't match
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
