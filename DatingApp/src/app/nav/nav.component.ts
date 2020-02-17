44ff976740caa6eeecff83e0dd5f660da7a3b37e
aaronrcs <aromer61@calstatela.edu>
2020-02-16 23:41:36 -0800

    Created an Auth Guard to prevent user from navigating to unauthorized routes

--- DatingApp/src/app/nav/nav.component.ts
+++ DatingApp/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { AlertifyService } from './../_services/alertify.service';
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,14 +12,15 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
+    this.authService.login(this.model).subscribe(response => {
       this.alertify.success('Logged in Successfully!');
+      this.router.navigate(['/members']);
     }, error => {
       this.alertify.error(error);
     });
@@ -31,6 +33,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out!');
+    this.router.navigate(['/home']);
   }
 
 }
--- DatingApp/src/app/routes.ts
+++ DatingApp/src/app/routes.ts
@@ -3,10 +3,11 @@ import { MessagesComponent } from './messages/messages.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent },
+    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
     { path: 'messages', component: MessagesComponent },
     { path: 'lists', component: ListsComponent },
     // Using wildcard path in case other paths don't match
