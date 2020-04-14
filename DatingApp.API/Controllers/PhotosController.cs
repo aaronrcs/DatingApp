fc2063201fe476104ecc5b720a787886995908e7
aaronrcs <aromer61@calstatela.edu>
2020-04-13 21:52:29 -0700

    Completed functionality for adding photouser in Photos Controller, now I need to test it in Postman

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -40,6 +40,16 @@ namespace DatingApp.API.Controllers
             _cloudinary = new Cloudinary(acc);
 
         }
+        [HttpGet("{id}", Name = "GetPhoto")]
+
+        public async Task<IActionResult> GetPhoto(int id)
+        {
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
 
         [HttpPost]
         public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto) 
@@ -85,7 +95,9 @@ namespace DatingApp.API.Controllers
 
             if(await _repo.SaveAll())
             {
-                return Ok();
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto", new { userId = userId, id = photo.Id},
+                photoToReturn);
             }
 
             return BadRequest("Could not add the photo");
