9387fe7b844a96d5bfeaf38de99d9c201e9e1496
aaronrcs <aromer61@calstatela.edu>
2020-04-29 21:40:13 -0700

    Added HttpDelete method in dotnet Apiand added the necessary functions in the client side to allow users to remove photos the uploaded

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -136,5 +136,49 @@ namespace DatingApp.API.Controllers
             return BadRequest("Could not set photo to main");
 
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
+                return Unauthorized();
+            }
+
+            var user = await _repo.GetUser(userId);
+
+            if(!user.Photos.Any(p => p.Id == id)){
+                return Unauthorized();
+            }
+
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            if(photoFromRepo.IsMain){
+                return BadRequest("You cannot delete your main photo");
+            }
+
+            // PhotoId - could also be seen as the PublicId of the photo that was uploaded by user
+
+            if (photoFromRepo.PhotoId != null) {
+
+                var deleteParams = new DeletionParams(photoFromRepo.PhotoId);
+
+                var result = _cloudinary.Destroy(deleteParams);
+
+                if (result.Result == "ok") {
+                    _repo.Delete(photoFromRepo);
+                }
+
+            }
+
+            if (photoFromRepo.PhotoId == null) {
+                 _repo.Delete(photoFromRepo);
+            }
+
+            if (await _repo.SaveAll()){
+                return Ok();
+            }
+
+            return BadRequest("Failed to delete the photo");
+        }
     }
 }
