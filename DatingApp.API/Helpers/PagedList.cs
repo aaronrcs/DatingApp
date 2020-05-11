73bbc39c6812d765d918c6d771c867b0cda33c58
aaronrcs <aromer61@calstatela.edu>
2020-05-11 14:44:25 -0700

    Created a PageList class to help assist with paging in API

---
+++ DatingApp.API/Helpers/PagedList.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Helpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        // PageList contructor with a list of paramaters that will be passed to this class via new instance 
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, 
+            int pageNumber, int pageSize)
+        {
+            // Counting the number of elements
+            var count = await source.CountAsync();
+            // Get items from source; Use Paging information to determine how items we will return
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
