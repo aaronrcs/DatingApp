ec5287ba7cd7d3c1c4e94622924bd9c414af5808
aaronrcs <aromer61@calstatela.edu>
2020-05-27 14:40:31 -0700

    Added functionlaity to allow users to like other users via client side. User will also have a notification of when they liked a user

--- DatingApp/src/app/members/member-list/member-card/member-card.component.ts
+++ DatingApp/src/app/members/member-list/member-card/member-card.component.ts
@@ -1,3 +1,6 @@
+import { AlertifyService } from './../../../_services/alertify.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from './../../../_services/auth.service';
 import { User } from './../../../_models/user';
 import { Component, OnInit, Input } from '@angular/core';
 
@@ -10,9 +13,17 @@ export class MemberCardComponent implements OnInit {
 
   @Input() user: User;
 
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+  }
+
+  sendlike(id: number) {
+    this.userService.sendLike(this.authService.decodedToken.nameid, id).subscribe(data => {
+      this.alertify.success('You have liked: ' + this.user.knownAs);
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
